e7eba2a4889e64add309517bf20a973a121353fb
PabloHerreros <pablo@climasens.com>
2022-05-10 11:37:22 -0400

    Index completed - there are now two different ways to construct an index present

--- src/FHVI_index_build.R
+++ src/FHVI_index_build.R
@@ -65,16 +65,25 @@ indicators_FHVI <- indicators_FHVI %>%
          EXP_RES = normalize(EXP_ExpHUnits, output_range = c(0,100)),
          EXP_ROAD = normalize(EXP_ExpRoadArea, output_range = c(0,100)),
          EXP_SITES = normalize(EXP_PSites, output_range = c(0,100))) %>%
-  mutate(HV_n = normalize(HV),
-         SEV_n = normalize(SEV),
-         HoV_n = normalize(HoV),
-         EXP_RES_n = normalize(EXP_RES),
-         EXP_ROAD_n = normalize(EXP_ROAD),
-         EXP_SITES_n = normalize(EXP_SITES))
+  mutate(HV_n = normalize(HV, output_range = c(0, 100)),
+         SEV_n = normalize(SEV, output_range = c(0, 100)),
+         HoV_n = normalize(HoV, output_range = c(0, 100)),
+         EXP_RES_n = normalize(EXP_RES, output_range = c(0, 100)),
+         EXP_ROAD_n = normalize(EXP_ROAD, output_range = c(0, 100)),
+         EXP_SITES_n = normalize(EXP_SITES, output_range = c(0, 100))) %>%
+  mutate(HV_Q5 = quintile_label(., "HV"),
+         SEV_Q5 = quintile_label(., "SEV"),
+         HoV_Q5 = quintile_label(., "HoV"),
+         EXP_RES_Q5 = quintile_label(., "EXP_RES"),
+         EXP_ROAD_Q5 = quintile_label(., "EXP_ROAD"),
+         EXP_SITES_Q5 = quintile_label(., "EXP_NSites")) %>%
+  mutate(V_n_sum = (HV_n + SEV_n + HoV_n)/3,
+         EXP_n_sum = (EXP_RES_n + EXP_ROAD_n)/2) %>%
+  mutate(V_Q5_sum = (HV_Q5 + SEV_Q5 + HoV_Q5)/(5*3),
+         EXP_Q5_sum = (EXP_RES_Q5 + EXP_ROAD_Q5)/(5*2)) %>%
+  mutate(V_x_EXP_Q5 = V_Q5_sum * EXP_Q5_sum,
+         V_x_EXP_n = V_n_sum * EXP_n_sum)
 
-indicators_FHVI$HV_Q5 <- quintile_label(indicators_FHVI, "HV")
-indicators_FHVI$SEV_Q5 <- quintile_label(indicators_FHVI, "SEV")
-indicators_FHVI$HoV_Q5 <- quintile_label(indicators_FHVI, "HoV")
-indicators_FHVI$EXP_RES_Q5 <- quintile_label(indicators_FHVI, "EXP_RES")
-indicators_FHVI$EXP_ROAD_Q5 <- quintile_label(indicators_FHVI, "EXP_ROAD")
-indicators_FHVI$EXP_SITES_Q5 <- quintile_label(indicators_FHVI, "EXP_NSites")
+st_write(indicators_FHVI, 
+         "data/output/final_FHVI.geojson",
+         delete_dsn = TRUE)
